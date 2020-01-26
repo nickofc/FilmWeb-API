9d80f630807791682127fbb2db5ee6b97b149ac5
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-01-26 13:43:47 +0100

    add get film votes avg

---
+++ src/FilmWebAPI/Requests/Get/GetFilmVotesAvg.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FilmWebAPI.Requests.Get
+{
+    class GetFilmVotesAvg : RequestBase<double>
+    {
+        private const int AVG_RATE_INDEX = 2;
+
+        public GetFilmVotesAvg(ulong movieId)
+            : base(Signature.Create("getFilmInfoFull", movieId), FilmWebHttpMethod.Get)
+        {
+        }
+
+        public override async Task<double> Parse(HttpResponseMessage responseMessage)
+        {
+            var jsonBody = await base.GetJsonBody(responseMessage);
+            var json = JsonConvert.DeserializeObject<JArray>(Regex.Replace(jsonBody, "t(s?):(\\d+)$", string.Empty));
+
+            var avgRate = json[AVG_RATE_INDEX].ToString();
+            return double.Parse(avgRate);
+        }
+    }
+}
