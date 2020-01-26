e036acb2da046c7ab9a73e3659e5cf879c05f023
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-01-26 14:51:39 +0100

    Get movie titles (#5)
    * get polish title
    
    * get original title
    
    * correct names

---
+++ src/FilmWebAPI/Requests/Get/GetFilmOriginalTitle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FilmWebAPI.Requests.Get
+{
+    class GetFilmOriginalTitle : RequestBase<string>
+    {
+        private const int ORIGINAL_TITLE_INDEX = 1;
+
+        public GetFilmOriginalTitle(ulong movieId)
+            : base(Signature.Create("getFilmInfoFull", movieId), FilmWebHttpMethod.Get)
+        {
+        }
+
+        public override async Task<string> Parse(HttpResponseMessage responseMessage)
+        {
+            var jsonBody = await base.GetJsonBody(responseMessage);
+            var json = JsonConvert.DeserializeObject<JArray>(Regex.Replace(jsonBody, "t(s?):(\\d+)$", string.Empty));
+
+            return json[ORIGINAL_TITLE_INDEX].ToString();
+        }
+    }
+}
